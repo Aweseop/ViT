525e5e26b826d64d55d8e5b273858978f7e36e8c
Aweseop <87693860+Aweseop@users.noreply.github.com>
2022-07-05 16:37:56 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python main.py --in_channel 1 --img_size 28 --patch_size 4 --emb_dim 4*4 --n_layers 6 --num_heads 2 --forward_dim 4 --dropout_ratio 0.1 --n_classes 10
+python main.py --batch_size 64 --epochs 10 --n_fold 5 --in_channel 1 --img_size 28 --patch_size 4 --emb_dim 4*4 --n_layers 6 --num_heads 2 --forward_dim 4 --dropout_ratio 0.1 --n_classes 10
